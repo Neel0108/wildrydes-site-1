eb53ce9f9fd6010c8559c3ea8ac50603a9699f7d
Neel0108 <101621871+Neel0108@users.noreply.github.com>
2022-08-26 12:04:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "ap-south-1_vFfOIQFK4", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "529t1l02u7mai0srhgrttq9otc", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
